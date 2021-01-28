9c2c67c6ba31e1d334c967c0f8dc94bc3969ba96
Caiptain-Mo <65597620+Caiptain-Mo@users.noreply.github.com>
2021-01-28 20:02:39 +0100

    improve  the footer

--- frontend/src/components/Footer.svelte
+++ frontend/src/components/Footer.svelte
@@ -9,11 +9,7 @@
   class="relative bottom-0 left-0 right-0 min-w-full p-4 flex flex-col justify-center justify-items-center text-center content-center font-sans text-xs sm:text-base md:font-medium bg-indigo-500 text-white"
 >
   <div class="justify-center justify-items-center">
-    &copy;
-    {year}&nbsp;
-  </div>
-  <div class="justify-center justify-items-center">
-    &nbsp;<a href="." class="underline">Policy</a>
+   Copyright &copy;{year}&nbsp;&nbsp;<a href="/imprint" class="">Imprint</a>
   </div>
 </footer>
 
