56436324017b101e69b521546e50d55ac2af9630
Dietrich <dietrich.poensgen@heg-portal.de>
2021-01-30 09:35:56 +0100

    finish mvp version of backend

--- backend/index.js
+++ backend/index.js
@@ -2,30 +2,50 @@ const express  = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 require('./config');
 
-const { Session } = require('./models');
+const { Contribution, Session } = require('./models');
   
 const typeDefs = gql`
+    type Contribution {
+        author: String!
+        content: String!
+        sessionID: String! 
+    }
+
     type Session {
         id: ID!
         code: String!
   description: String!
+  contributions: [Contribution!]!
     }
+
     type Query {
         getSessions: [Session!]
+        getContributions(sessionID: String!): [Contribution!]
     }
+
     type Mutation {
         addSession(code: String!, description: String!): Session
+        addContribution(author: String!, content: String!, sessionID: String): Contribution
     }
 `;
 
 const resolvers = {
     Query: {
-        getSessions: async () => await Session.find({}).exec()
+        getSessions: async () => await Session.find({}).exec(),
+        getContributions: async () => await Contribution.find({}).exec()
     },
     Mutation: {
         addSession: async (_, args) => {
             try {
                 let response = await Session.create(args);
+                return response;
+            } catch(e) {
+                return e.message;
+            }
+        },
+        addContribution: async (_, args) => {
+            try {
+                let response = await Contribution.create(args);
                 return response;
             } catch(e) {
                 return e.message;
--- backend/models.js
+++ backend/models.js
@@ -2,12 +2,20 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const sessionSchema = new Schema({
-  code:{type: String, unique:true},
+  code:{type: String, unique:true, minlength: 6, maxlength: 6},
   description: String
 });
 
+const contributionSchema = new Schema({
+  author: String,
+  content: String,
+  sessionID: String
+});
+
 const Session = mongoose.model('session', sessionSchema); 
+const Contribution = mongoose.model('contribution', contributionSchema)
 
 module.exports = {
-  Session
+  Session,
+  Contribution
 };
