b4e4f2377ebbde9a1b25187d414d3183ad7780ed
Caiptain-Mo <65597620+Caiptain-Mo@users.noreply.github.com>
2021-02-04 15:36:58 +0100

    Update imprint.svelte

--- frontend/src/pages/imprint.svelte
+++ frontend/src/pages/imprint.svelte
@@ -1,2 +1,76 @@
-<h1 class="text-3xl font-medium">Imprint</h1>
- 
+<script>
+    var date = new Date();
+    var year = date.getFullYear();
+  </script>
+
+
+<div class="text-black dark:text-white">
+    <div class="container">
+        <p class="text-4xl">Impressum</p>
+        <br>
+        <p class="subtitle is-2">Angaben gemäß § 5 TMG</p>
+        <br>
+        <strong>Verantwortlich</strong>
+        <p>Moritz Kelle</p>
+        <p>Dietrich Poensgen</p>
+        <br>
+        <strong>Kontaktdaten</strong>
+        <br>
+        <p>Eduteam GMBH</p>
+        <p>Mustermann Straße 34</p>
+        <p>29524 Musterhausen</p>
+        <br>
+        <p>Email:</p><a href= "mailto:contact@eduteam.de">contact@eduteam.de</a>
+        <p>Telefon: 05822 987654</p>
+        <br><br>
+        <p class="subtitle is-2">Copyright </p>
+        <strong>© Copyright {year} – Urheberrechtshinweis</strong>
+        <br><br>
+        <p>Alle Inhalte dieses Internetangebotes, insbesondere Texte, Fotografien und Grafiken, sind urheberrechtlich geschützt. Das Urheberrecht liegt, soweit nicht ausdrücklich anders gekennzeichnet,</p><a href="#Lizenzen">(Lizenzen)</a><p>bei Moritz Kelle und Dietrich Poensgen. Bitte fragen Sie uns, falls Sie die Inhalte dieses Internetangebotes verwenden möchten.</p>
+        <br>
+        <p>Unter der „Creative Commons“-Lizenz“ veröffentlichte Inhalte, sind als solche gekennzeichnet. Sie dürfen entsprechend den angegebenen Lizenzbedingungen verwendet werden.</p>
+        <br>
+        <p>Wer gegen das Urheberrecht verstößt (z.B. Bilder oder Texte unerlaubt kopiert), macht sich gem. §§ 106 ff UrhG strafbar, wird zudem kostenpflichtig abgemahnt und muss Schadensersatz leisten (§ 97 UrhG).</p>
+        <br><br>
+        <p class="subtitle is-2">Rechtliches</p>
+        <strong> Haftung für Inhalte</strong>
+        <br>
+        <p>Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+        <br> <br>
+        <strong>Haftung für Links</strong>
+        <br> 
+        <p>Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
+        <br><br>
+        <strong> Datenschutz</strong>
+        <br>
+        <p>Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.</p> 
+        <br>
+        <p>Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.</p>
+        <br>
+        <p>Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.</p>
+        <br><br>
+        
+        <section id="Lizenzen">
+            <p class="subtitle is-2">Lizenzen</p>
+            <br>
+            <p class="subtitle is-3">Icons</p>
+            <p>Alle in diesem Internet Angebot verwendetem Icons stammen  von</p><strong>„Ionicons“</strong><a href="https://ionicons.com/">(https://ionicons.com/)</a>   
+            <br><br>
+            <strong>Released under MIT License</strong>
+            <br><br>
+            <strong>Copyright (c) 2020 IONIC</strong>
+            <br>
+            <p>
+                Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+                <br>
+                The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+                <br>
+                The Software is provided “as is”, without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the Software.
+            </p>
+            <br><br>
+            <p class="subtitle is-3">CSS Framework</p>
+            <p>Als CSS Framework verwenden wir</p><strong>„Tailwind“</strong><a href="https://tailwindcss.com/" class="text-blue-500">(Hier gehts zu tailwindcss)</a>   
+        </section>  
+
+    </div> 
+</div>
