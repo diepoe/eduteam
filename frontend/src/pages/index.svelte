475d11ce7444f468315de07c7a40e2910cea9ae6
Caiptain-Mo <65597620+Caiptain-Mo@users.noreply.github.com>
2021-02-05 13:03:32 +0100

    Update index.svelte

--- frontend/src/pages/index.svelte
+++ frontend/src/pages/index.svelte
@@ -7,7 +7,7 @@
   let code = "";
 </script>
 
-<h1 class="text-5xl font-medium  object-center">Eduteam</h1>
+<h1 class="text-5xl font-medium  text-center">Eduteam</h1>
 
 <div class="flex justify-center align-middle mt-4 p-4 md:p-6 md:mt-8 space-x-2">
   <input
@@ -16,9 +16,9 @@
     maxlength="6"
     bind:value={code}
     placeholder="Enter your code..."
-    class="w-3/5 m-1 self-center transform scale-150 duration-300 rounded-xl border-indigo-500 border-3 focus:scale-160 focus:border-8 hover:border-indigo-600 dark:bg-gray-700"
+    class="w-3/5 m-1 self-center duration-300 rounded-xl border-indigo-500 border-3 focus:border-8 hover:border-indigo-600 dark:bg-gray-700"
   />
-  <br>
+
   <a
     href="/session/{code}"
     class="transform duration-300 p-3 bg-gradient-to-r from-indigo-500 to-indigo-600 hover:from-indigo-600 hover:to-indigo-700 hover:scale-110 text-white rounded-xl self-center"
