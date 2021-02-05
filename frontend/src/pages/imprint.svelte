d34a21ff26501dce93519839b83f7b211d3d0677
Dietrich <dietrich.poensgen@heg-portal.de>
2021-02-05 17:31:08 +0100

    update imprint // make it suitable for darkmode

--- frontend/src/pages/imprint.svelte
+++ frontend/src/pages/imprint.svelte
@@ -1,76 +1,148 @@
 <script>
-    var date = new Date();
-    var year = date.getFullYear();
-  </script>
+  var date = new Date();
+  var year = date.getFullYear();
+</script>
 
+<div class="prose md:prose-lg text-black dark:text-white">
+  <div class="container">
+    <h1 class="dark:text-white">Impressum</h1>
+    <h2 class="dark:text-white">Angaben gemäß § 5 TMG</h2>
+    <h4 class="dark:text-white">Verantwortlich</h4>
+    <p>Moritz Kelle</p>
+    <p>Dietrich Poensgen</p>
+    <br />
+    <h4 class="dark:text-white">Kontaktdaten</h4>
+    <p>Eduteam GMBH</p>
+    <p>Mustermann Straße 34</p>
+    <p>29524 Musterhausen</p>
 
-<div class="text-black dark:text-white">
-    <div class="container prose">
-        <h1>Impressum</h1>
-        <br>
-        <h2>Angaben gemäß § 5 TMG</h2>
-        <br>
-        <strong>Verantwortlich</strong>
-        <p>Moritz Kelle</p>
-        <p>Dietrich Poensgen</p>
-        <br>
-        <strong>Kontaktdaten</strong>
-        <br>
-        <p>Eduteam GMBH</p>
-        <p>Mustermann Straße 34</p>
-        <p>29524 Musterhausen</p>
-        <br>
-        <p>Email:</p><a href= "mailto:contact@eduteam.de">contact@eduteam.de</a>
-        <p>Telefon: 05822 987654</p>
-        <br><br>
-        <h2>Copyright </h2>
-        <strong>© Copyright {year} – Urheberrechtshinweis</strong>
-        <br><br>
-        <p>Alle Inhalte dieses Internetangebotes, insbesondere Texte, Fotografien und Grafiken, sind urheberrechtlich geschützt. Das Urheberrecht liegt, soweit nicht ausdrücklich anders gekennzeichnet,</p><a href="#Lizenzen">(Lizenzen)</a><p>bei Moritz Kelle und Dietrich Poensgen. Bitte fragen Sie uns, falls Sie die Inhalte dieses Internetangebotes verwenden möchten.</p>
-        <br>
-        <p>Unter der „Creative Commons“-Lizenz“ veröffentlichte Inhalte, sind als solche gekennzeichnet. Sie dürfen entsprechend den angegebenen Lizenzbedingungen verwendet werden.</p>
-        <br>
-        <p>Wer gegen das Urheberrecht verstößt (z.B. Bilder oder Texte unerlaubt kopiert), macht sich gem. §§ 106 ff UrhG strafbar, wird zudem kostenpflichtig abgemahnt und muss Schadensersatz leisten (§ 97 UrhG).</p>
-        <br><br>
-        <h2>Rechtliches</h2>
-        <strong> Haftung für Inhalte</strong>
-        <br>
-        <p>Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
-        <br> <br>
-        <strong>Haftung für Links</strong>
-        <br> 
-        <p>Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
-        <br><br>
-        <strong> Datenschutz</strong>
-        <br>
-        <p>Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.</p> 
-        <br>
-        <p>Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.</p>
-        <br>
-        <p>Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.</p>
-        <br><br>
-        
-        <section id="Lizenzen">
-            <h2>Lizenzen</h2>
-            <br>
-            <h3>Icons</3>
-            <p>Alle in diesem Internet Angebot verwendetem Icons stammen von</p><strong>„Tabler Icons“</strong><a href="https://tabler-icons.io/">Tabler Icons</a>   
-            <br><br>
-            <strong>Released under MIT License</strong>
-            <br><br>
-            <strong>Copyright (c) 2021 codecalm.net</strong>
-            <br>
-            <p>
-                Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-                <br>
-                The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-                <br>
-                The Software is provided “as is”, without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the Software.
-            </p>
-            <br><br>
-            <h3>CSS Framework</h3>
-            <p>Als CSS Framework verwenden wir <a href="https://tailwindcss.com/"><strong>„Tailwind“</strong></a>  </p> 
-        </section>  
+    <h4 class="dark:text-white">Email:</h4>
+    <a href="mailto:contact@eduteam.de" class="dark:text-white"
+      >contact@eduteam.de</a
+    >
+    <br />
+    <h2 class="dark:text-white">Copyright</h2>
+    <h4 class="dark:text-white">© Copyright {year} – Urheberrechtshinweis</h4>
+    <p>
+      Alle Inhalte dieses Internetangebotes, insbesondere Texte, Fotografien und
+      Grafiken, sind urheberrechtlich geschützt. Das Urheberrecht liegt, soweit
+      nicht ausdrücklich anders gekennzeichnet,
+    </p>
+    <a href="#Lizenzen" class="dark:text-white">(Lizenzen)</a>
+    <p>
+      bei Moritz Kelle und Dietrich Poensgen. Bitte fragen Sie uns, falls Sie
+      die Inhalte dieses Internetangebotes verwenden möchten.
+    </p>
 
-    </div> 
+    <p>
+      Unter der „Creative Commons“-Lizenz“ veröffentlichte Inhalte, sind als
+      solche gekennzeichnet. Sie dürfen entsprechend den angegebenen
+      Lizenzbedingungen verwendet werden.
+    </p>
+
+    <p>
+      Wer gegen das Urheberrecht verstößt (z.B. Bilder oder Texte unerlaubt
+      kopiert), macht sich gem. §§ 106 ff UrhG strafbar, wird zudem
+      kostenpflichtig abgemahnt und muss Schadensersatz leisten (§ 97 UrhG).
+    </p>
+    <br />
+    <h2 class="dark:text-white">Rechtliches</h2>
+    <h4 class="dark:text-white">Haftung für Inhalte</h4>
+
+    <p>
+      Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die
+      Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch
+      keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG
+      für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen
+      verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch
+      nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu
+      überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
+      Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der
+      Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon
+      unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt
+      der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden
+      von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend
+      entfernen.
+    </p>
+    <br />
+    <h4 class="dark:text-white">Haftung für Links</h4>
+    <p>
+      Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren
+      Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden
+      Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten
+      Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten
+      verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung
+      auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum
+      Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche
+      Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte
+      einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von
+      Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+    </p>
+    <br />
+    <h4 class="dark:text-white">Datenschutz</h4>
+    <p>
+      Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name,
+      Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit
+      möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre
+      ausdrückliche Zustimmung nicht an Dritte weitergegeben.
+    </p>
+
+    <p>
+      Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der
+      Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein
+      lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht
+      möglich.
+    </p>
+
+    <p>
+      Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten
+      Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich
+      angeforderter Werbung und Informationsmaterialien wird hiermit
+      ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich
+      ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von
+      Werbeinformationen, etwa durch Spam-Mails, vor.
+    </p>
+    <br />
+    <section id="Lizenzen">
+      <h2 class="dark:text-white">Lizenzen</h2>
+      <br />
+      <h3 class="dark:text-white">Icons</h3>
+      <p>Alle in diesem Internet Angebot verwendetem Icons stammen von</p>
+      <h4 class="dark:text-white">„Tabler Icons“</h4>
+      <a class="dark:text-white" href="https://tabler-icons.io/">Tabler Icons</a
+      >
+      <br /><br />
+      <h4 class="dark:text-white">Released under MIT License</h4>
+      <br /><br />
+      <h4 class="dark:text-white">&copy; {year} codecalm.net</h4>
+      <br />
+      <p>
+        Permission is hereby granted, free of charge, to any person obtaining a
+        copy of this software and associated documentation files (the
+        “Software”), to deal in the Software without restriction, including
+        without limitation the rights to use, copy, modify, merge, publish,
+        distribute, sublicense, and/or sell copies of the Software, and to
+        permit persons to whom the Software is furnished to do so, subject to
+        the following conditions:
+        <br />
+        The above copyright notice and this permission notice shall be included in
+        all copies or substantial portions of the Software.
+        <br />
+        The Software is provided “as is”, without warranty of any kind, express or
+        implied, including but not limited to the warranties of merchantability,
+        fitness for a particular purpose and noninfringement. In no event shall the
+        authors or copyright holders be liable for any claim, damages or other liability,
+        whether in an action of contract, tort or otherwise, arising from, out of
+        or in connection with the software or the use or other dealings in the Software.
+      </p>
+      <br /><br />
+      <h3 class="dark:text-white">CSS Framework</h3>
+      <p>
+        Als CSS Framework verwenden wir <a
+          href="https://tailwindcss.com/"
+          class="dark:text-white">tailwindcss</a
+        >
+      </p>
+    </section>
+  </div>
 </div>
