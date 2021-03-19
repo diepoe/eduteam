3603b624bf03d33fe462b2df845bb3f2d171af0c
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2021-03-19 16:16:04 +0100

    deploy backend

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,10 @@ const SessionSchema = require("./lists/Session");
 
 const keystone = new Keystone({
   adapter: new Adapter(adapterConfig),
+  cookie: {
+    secure: process.env.NODE_ENV === "production",
+  },
+  cookieSecret: process.env.COOKIE_SECRET || "very-secret",
 });
 
 keystone.createList("Contribution", ContributionSchema);
@@ -24,9 +28,26 @@ keystone.createList("Contribution", ContributionSchema);
 keystone.createList("Session", SessionSchema);
 
 keystone.createList("User", {
+  access: {
+    read: ({ authentication: { item } }) => {
+      return {
+        state_not: "deactivated",
+      };
+    },
+  },
   fields: {
     username: { type: Text },
-    password: { type: Password },
+    password: {
+      type: Password,
+      access: {
+        // 3. Only admins can see if a password is set. No-one can read their own or other user's passwords.
+        read: ({ authentication }) => authentication.item.isAdmin,
+        // 4. Only authenticated users can update their own password. Admins can update anyone's password.
+        update: ({ existingItem, authentication: { item } }) => {
+          return item.isAdmin || existingItem.id === item.id;
+        },
+      },
+    },
   },
 });
 
